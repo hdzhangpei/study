868301a819e99f27c8c710dd13600639e7734085
pei.zhang <pei.zhang@7fresh.com>
2019-06-03 12:45:21 +0800

    [验证守护线程中的finally在所有非守护线程执行完毕时|当系统exit()时,会强制所有守护进程.导致finally代码未执行]-张培

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 base包下:基础实验
 ```
 ##### 2019-06-03
-* 新增单例模式不同实现:线程不安全,线程安全低效,线程安全高效(推荐),枚举单例(推荐):com.zhangpei.study.base.singleton
+* 新增单例模式不同实现:线程不安全,线程安全低效,线程安全高效(推荐),枚举单例(推荐)
+com.zhangpei.study.base.singleton
+* 验证守护线程中的finally在所有非守护线程执行完毕时|当系统exit()时,会强制所有守护进程.导致finally代码未执行
+com.zhangpei.study.base.daemon
---
+++ src/main/java/com/zhangpei/study/base/daemon/DaemonUnInvokeFinally.java
@@ -0,0 +1,66 @@
+package com.zhangpei.study.base.daemon;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 验证守护线程中的finally在所有非守护线程执行完毕时|当系统exit()时,会强制所有守护进程.导致finally代码未执行
+ */
+public class DaemonUnInvokeFinally {
+    public static void main(String[] args) {
+        //验证守护线程中的finally在所有非守护线程执行完毕时,会强制所有守护进程.导致finally代码未执行
+        testDaemonUnInvokeFinally();
+
+        ////验证守护线程中的finally当系统exit()时,会强制所有守护进程.导致finally代码未执行
+//        testSystemExitUNInvokeFinally();
+    }
+
+    private static void testSystemExitUNInvokeFinally() {
+        Thread t2 = new Thread(() -> {
+            try {
+                synchronized(DaemonUnInvokeFinally.class) {
+                    System.out.println("t2 enter try");
+                    TimeUnit.SECONDS.sleep(5);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("t2 enter finally");
+            }
+        });
+
+        t2.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.exit(0);
+    }
+
+    private static void testDaemonUnInvokeFinally() {
+        Thread t1 = new Thread(() -> {
+            try {
+                synchronized(DaemonUnInvokeFinally.class) {
+                    System.out.println("t1 enter try");
+                    TimeUnit.SECONDS.sleep(5);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("t1 enter finally");
+            }
+        });
+
+        // 设置t1为守护线程
+        t1.setDaemon(true);
+        t1.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
