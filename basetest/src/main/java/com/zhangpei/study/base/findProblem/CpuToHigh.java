6183c8ec0573ccc97cf0248d31860f08476ca2aa
pei.zhang <pei.zhang@7fresh.com>
2020-04-08 20:54:36 +0800

    [cpu]-张培

--- basetest/src/main/java/com/zhangpei/study/base/findProblem/CpuToHigh.java
+++ basetest/src/main/java/com/zhangpei/study/base/findProblem/CpuToHigh.java
@@ -1,11 +1,18 @@
 package com.zhangpei.study.base.findProblem;
 
+import com.zhangpei.study.base.algorithm.IntTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CpuToHigh {
-    public static void main(String[] args) {
-        int random = 0 ;
-        while(random < 100) {
-            System.out.println(random);
-            random = random * 10;
+    public static void main(String[] args) throws InterruptedException {
+
+        List<IntTest> list = new ArrayList<>();
+
+        while(true) {
+            Thread.sleep(100);
+            System.out.println(11);
         }
     }
 }
